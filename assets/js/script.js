1564dc426f1e30340c5f942c4eca3818fde5fe47
Charlie Madison <prowebscape@gmail.com>
2011-02-22 23:08:58 -0600

    Version 1

---
+++ assets/js/script.js
@@ -0,0 +1,132 @@
+COMMON = {
+    equal_height: function(group) {
+    	var tallest = 0;
+    	group.each(function() {
+    		var thisHeight = $(this).height();
+    		if(thisHeight > tallest) {
+    			tallest = thisHeight;
+    		}
+    	});
+    	group.height(tallest);
+    },
+    
+    superfish: function(parent) {
+    $(parent) //ul#main-menu
+        .supersubs({
+            maxWidth:    35,
+            minWidth:    15
+        })
+        .superfish({
+            animation:   {opacity:'show',height:'show'},
+            easing:      'easeOutBack'
+        })
+        .find('ul').bgIframe({opacity:false});
+    },
+    
+    gallery: function() {
+      $("a[rel='gallery']").colorbox({
+          innerWidth: '550px'
+          });
+    },
+    
+    js_back: function() {
+      $('a.js_back').click(function(e) {
+          history.go(-1);
+          e.preventDefault();
+      });
+    },
+    
+    hover_image: function() {
+      $('a').hover(
+        function(event) {
+          var img = $(this).find('img:first'),
+              hover = $(img).data('hover'),
+              src   = $(img).attr('src');
+          
+          if( img === undefined || hover === undefined || src === undefined) {
+            return;
+          }
+          
+          $(img).data('original_src', src);
+          $(img).attr('src', hover);
+        },
+        function(event) {
+          var img = $(this).find('img:first'),
+              hover = $(img).attr('src'),
+              src   = $(img).data('original_src');
+          
+          if( img === undefined || hover === undefined || src === undefined) {
+            return;
+          }
+          
+          $(img).attr('src', src);          
+        }
+      )
+    },
+    
+    iframe_colorbox: function() {
+      $("a.iframe-colorbox").colorbox({
+        width:"97%", 
+        height:"97%", 
+        iframe:true
+      });
+    }
+}
+
+SITE = {
+  common: {
+    init: function() {
+      // application-wide code
+      COMMON.hover_image();
+      COMMON.iframe_colorbox();
+      COMMON.js_back();
+    },
+
+    run_after: function() {
+      /* Do this after Controller:Action action has run */
+    }
+  },
+  
+  home: {
+    init: function() {
+    },
+    
+    /* document w/ id 1 */    
+    doc1: function() {
+      /* do something on this page only */
+    },
+    
+    run_after: function() {
+      /* Do this after the common::run_after and Controller:Action action has run */
+    }
+  }
+};
+
+UTIL = {
+  exec: function( controller, action ) {
+    var ns = SITE,
+        action = ( action === undefined ) ? "init" : action;
+
+    if ( controller !== "" && ns[controller] && typeof ns[controller][action] == "function" ) {
+      ns[controller][action]();
+    }
+  },
+
+  init: function() {
+    var body = document.body,
+        controller = body.getAttribute( "data-template" ),
+        action = "doc" + body.getAttribute( "data-docid" );
+    
+    log(body, controller, action );
+    
+    UTIL.exec( "common" );
+    UTIL.exec( controller );
+    UTIL.exec( controller, action );
+    UTIL.exec( controller, "run_after" );
+    UTIL.exec( "common", "run_after" );
+  }
+};
+
+jQuery(document).ready(function($) {
+	UTIL.init();
+});
---
+++ dev.php
@@ -0,0 +1,117 @@
+<!doctype html>  
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ --> 
+<!--[if lt IE 7 ]> <html class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title></title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!--  Mobile viewport optimized: j.mp/bplateviewport -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Place favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+
+  <!-- CSS : implied media="all" -->
+  <link rel="stylesheet" href="assets/css/plugins.css?v=1">
+  <link rel="stylesheet" href="assets/css/style.css?v=1">
+  <link rel="stylesheet" href="assets/js/plugins/colorbox/v1/colorbox.css?v=1">
+ 
+  <!-- All JavaScript at the bottom, except for Modernizr which enables assets elements & feature detects -->
+  <script src="assets/js/libs/modernizr-1.6.min.js"></script>
+
+  <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+  <script>!window.jQuery && document.write(unescape('%3Cscript src="assets/js/libs/jquery-1.5.min.js"%3E%3C/script%3E'))</script>
+  <!-- end concatenated and minified scripts-->
+  
+  <!-- scripts concatenated and minified via ant build script-->
+  <script src="assets/js/plugins.js?v=1"></script>
+  <script src="assets/js/script.js?v=1"></script>
+  <!-- end concatenated and minified scripts-->
+
+</head>
+
+<body lang="en" data-template="home" data-docid="1">
+
+  <div id="container">
+    <header>
+      
+      <a id="logo" href="#"><span>Site Name Here</span></a>
+            
+      <nav>
+        <ul id="main-nav">
+          <li class="first"><a href="#">Home</a></li>
+          <li><a href="#">Item 1</a></li>
+          <li class="active"><a href="#">Item 2</a></li>
+          <li><a href="#">Item 3</a></li>
+          <li class="last"><a href="#">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+    
+    <div id="main" role="main" class="grid9 first">
+
+      <div class="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div><!-- e: content -->
+      
+    </div><!-- e: #main -->
+    
+
+    
+    <div id="side" class="grid3">
+            
+    </div><!-- e: #side -->
+    
+    <div class="clear">&nbsp;</div>
+    
+    <footer>
+      
+      <div class="copyright grid3">
+        Copyright &copy; 2009-2010. All rights reserved<br/>
+        Developed by: <a href="http://www.prowebscape.com">ProWebscape.com</a>
+      </div>
+      
+      <div class="footer-links grid9">
+        <ul id="footer-nav">
+          <li class="first"><a href="#">Home</a></li>
+          <li><a href="#">Item 1</a></li>
+          <li class="active"><a href="#">Item 2</a></li>
+          <li><a href="#">Item 3</a></li>
+          <li class="last"><a href="#">Contact Us</a></li>
+        </ul>
+      </div><!-- e: .footer-links -->
+      
+    </footer>
+  </div> <!--! end of #container -->
+
+
+  <!-- JavaScript at the bottom for fast page loading -->
+
+  <!--[if lt IE 7 ]>
+    <script src="js/libs/dd_belatedpng.js"></script>
+    <script>DD_belatedPNG.fix('img, .png_bg'); // Fix any <img> or .png_bg bg-images. Also, please read goo.gl/mZiyb </script>
+  <![endif]-->
+
+
+  <!-- mathiasbynens.be/notes/async-analytics-snippet Change UA-XXXXX-X to be your site's ID -->
+  <script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script>
+  
+</body>
+</html>
